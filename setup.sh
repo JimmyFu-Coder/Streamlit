bb48545ec5c123e9858be0ee877454e2a71df010
fu jinshuai <996367062@qq.com>
2023-08-14 07:58:43 +0800

    add Procfile

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
-mkdir -p ~/.streamlit
+mkdir -p ~/.streamlit/
 
-echo "
-[general]\n
-email = jinshuaifu@gmail.com
+echo "\
+[general]\n\
+email = \"jinshuaifu@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
-echo "
-[server]\n
-headless = true\n
-enableCORS=false\n
-port = $PORT
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
